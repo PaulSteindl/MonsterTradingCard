9268b71fd2df21fd33a95e356052c27e09d2f4b4
PaulSteindl <Paul.Steindl@live.com>
2022-01-20 17:28:17 +0100

    fix trading

--- MonsterTradingCard/RouteCommands/Trading/CreateTradingDeal.cs
+++ MonsterTradingCard/RouteCommands/Trading/CreateTradingDeal.cs
@@ -2,6 +2,8 @@
 using HTTPServerCore.Response.Response;
 using HTTPServerCore.Response.StatusCode;
 using MonsterTradingCard.Models.TradingDeal;
+using MonsterTradingCard.Models.Enums.CardType;
+using MonsterTradingCard.Models.Enums.Species;
 using PROT_ROUTE_COM = MonsterTradingCard.RouteCommands.ProtectedRouteCommand;
 using IMSGMANAGER = MonsterTradingCard.IMessageManager;
 
@@ -24,22 +26,30 @@ namespace MonsterTradingCard.RouteCommands.Trading.CreateTradingDealCommand
 
             if(messageManager.CheckCardAndUser(tradingDeal.CardToTrade, User.Token))
             {
-                if(!messageManager.CheckCardForTrade(tradingDeal.CardToTrade))
+                if ((tradingDeal.Type == CardType.monster && tradingDeal.Species != Species.none) || tradingDeal.Type == CardType.spell && tradingDeal.Species == Species.none)
                 {
-                    if(messageManager.CreateTradingdeal(tradingDeal, User.Token))
+                    if (!messageManager.CheckCardForTrade(tradingDeal.CardToTrade))
                     {
-                        response.Payload = "Deal wurde erstellt!";
-                        response.StatusCode = StatusCode.Ok;
+                        if (messageManager.CreateTradingdeal(tradingDeal, User.Token))
+                        {
+                            response.Payload = "Deal wurde erstellt!";
+                            response.StatusCode = StatusCode.Ok;
+                        }
+                        else
+                        {
+                            response.Payload = "Es kam zu einem Fehler beim erstellen des Deals!";
+                            response.StatusCode = StatusCode.InternalServerError;
+                        }
                     }
                     else
                     {
-                        response.Payload = "Es kam zu einem Fehler beim erstellen des Deals!";
-                        response.StatusCode = StatusCode.InternalServerError;
+                        response.Payload = "Karte wird schon gehandelt!";
+                        response.StatusCode = StatusCode.Conflict;
                     }
                 }
                 else
                 {
-                    response.Payload = "Karte wird schon gehandelt!";
+                    response.Payload = "Ungültige Typ - Species kombination!";
                     response.StatusCode = StatusCode.Conflict;
                 }
             }
